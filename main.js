1b900bc340b2eaa2babf4e99b8cdef87019dda90
kingsleyCarlson <kc_0701@hotmail.com>
2025-07-12 00:44:22 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const greeting = document.createElement("p");
+  greeting.textContent = "🎉 JavaScript is now working!";
+  document.body.appendChild(greeting);
+});
